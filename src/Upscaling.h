42fb676c88c4662baabe32aaf6bc80e1bc00370a
Tim <15017472+doodlum@users.noreply.github.com>
2025-03-02 21:32:13 +0000

    chore: default to VRR

--- src/Upscaling.h
+++ src/Upscaling.h
@@ -43,7 +43,7 @@ public:
 		uint upscaleMethodNoFSR = (uint)UpscaleMethod::kTAA;
 		float sharpness = 0.5f;
 		uint dlssPreset = (uint)sl::DLSSPreset::ePresetJ;
-		uint vsyncMode = 1;
+		uint vsyncMode = 0;
 		uint frameLimitMode = 1;
 		uint frameGenerationMode = 1;
 	};
