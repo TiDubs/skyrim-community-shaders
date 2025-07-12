470971fcafd2cd40b0e6d95a63d2ddbd101fbd85
Tim <15017472+doodlum@users.noreply.github.com>
2025-07-12 22:58:00 +0100

    refactor: rename var

--- src/TruePBR.cpp
+++ src/TruePBR.cpp
@@ -1232,8 +1232,8 @@ struct BSTempEffectSimpleDecal_SetupGeometry
 	{
 		func(decal, geometry, textureSet, blended);
 		auto* singleton = globals::truePBR;
-		auto uncastedProperty = geometry->GetGeometryRuntimeData().properties[1].get();
-		if (auto shaderProperty = uncastedProperty->GetRTTI() == globals::rtti::BSLightingShaderPropertyRTTI.get() ? static_cast<RE::BSLightingShaderProperty*>(uncastedProperty) : nullptr;
+		auto unknownProperty = geometry->GetGeometryRuntimeData().properties[1].get();
+		if (auto shaderProperty = unknownProperty->GetRTTI() == globals::rtti::BSLightingShaderPropertyRTTI.get() ? static_cast<RE::BSLightingShaderProperty*>(unknownProperty) : nullptr;
 			shaderProperty != nullptr && singleton->IsPBRTextureSet(textureSet)) {
 			{
 				BSLightingShaderMaterialPBR srcMaterial;
