40b859a9c3b0088cbc723b175b9bc6546d5aa1b5
Tim <15017472+doodlum@users.noreply.github.com>
2025-07-23 15:35:56 +0100

    chore: leave in asm patch

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1133,6 +1133,32 @@ namespace Hooks
 			stl::write_thunk_call<Main_Update_Swap>(REL::RelocationID(35565, 36564).address() + REL::Relocate(0x5D2, 0xA97));
 		}
 
+		
+		// Patch render space in BSLightingShader::SetupGeometry to always use world space
+		// The variable updateEyePosition is set to 1 when not skinned. By patching to be 0 it will always use world space
+		// We offset from the base address of the containing function to the start of the patch
+		{
+			logger::info("Patching BSLightingShader::SetupGeometry::updateEyePosition");
+			auto setupGeometryUpdateRenderSpace = REL::RelocationID(100565, 107300).address();
+
+			if (REL::Module::IsAE()) {
+				std::uint8_t patch[] = { 0x41, 0x83, 0xE7, 0x00 };  // and r15d, 0
+				REL::safe_write(setupGeometryUpdateRenderSpace + 0x71, patch, sizeof(patch));
+			} else if (REL::Module::IsVR()) {
+				std::uint8_t patch[] = { 0x41, 0x83, 0xE4, 0x00 };  // and r12d, 0
+				REL::safe_write(setupGeometryUpdateRenderSpace + 0x65, patch, sizeof(patch));
+			} else {
+				std::uint8_t patch1[] = { 0xB8, 0x00, 0x00 };  // mov eax, 0
+				REL::safe_write(setupGeometryUpdateRenderSpace + 0x73, patch1, sizeof(patch1));
+
+				std::uint8_t patch2[] = { 0x45, 0x31, 0xC9 };  // xor r9d, r9d (zeros r9d)
+				REL::safe_write(setupGeometryUpdateRenderSpace + 0x36D, patch2, sizeof(patch2));
+
+				std::uint8_t patch3[] = { 0x45, 0x31, 0xC0 };  // xor r8d, r8d (zeros r8d)
+				REL::safe_write(setupGeometryUpdateRenderSpace + 0x378, patch3, sizeof(patch3));
+			}
+		}
+
 		stl::write_thunk_call<BSLightingShader_SetupGeometry_GeometrySetupConstantPointLights>(REL::RelocationID(100565, 107300).address() + REL::Relocate(0x523, 0xB0E, 0x5FE));
 	}
 
